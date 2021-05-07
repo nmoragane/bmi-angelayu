82545cb7f48e67fccca9d80415d89f029687c862
nmoragane <nmoragane@gmail.com>
2021-05-07 22:50:10 +0530

    UI skeleton done

---
+++ lib/input_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('BMI CALCULATOR')),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                  child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Color(0xFF1D1E33),
+                ),
+              )),
+              Expanded(
+                  child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Color(0xFF1D1E33),
+                ),
+              )),
+            ],
+          )),
+          Expanded(
+              child: Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: Color(0xFF1D1E33),
+            ),
+          )),
+          Expanded(
+              child: Row(children: <Widget>[
+            Expanded(
+                child: Container(
+              margin: EdgeInsets.all(15.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Color(0xFF1D1E33),
+              ),
+            )),
+            Expanded(
+                child: Container(
+              margin: EdgeInsets.all(15.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Color(0xFF1D1E33),
+              ),
+            )),
+          ])),
+        ],
+      ),
+    );
+  }
+}
